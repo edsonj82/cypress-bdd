d9ac745dabfcec7ea6443c990025772430653679
Edson José dos Santos <edsonj82@outlook.com>
2024-02-29 11:41:43 -0300

    setup projetct (setting cucumber-preprocessor)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # cypress-bdd
+
+- npm install cypress
+- npx cypress install --force  (reinstalar)
+- npx cypress open
+
+- $ npm install @badeball/cypress-cucumber-preprocessor (https://github.com/badeball/cypress-cucumber-preprocessor/blob/master/docs/quick-start.md)
+
+- npm install @bahmutov/cypress-esbuild-preprocessor
---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor")
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor")
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild")
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    setupNodeEvents(on, config) {
+      addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      // Make sure to return the config object as it might have been modified by the plugin.
+      return config;
+    },
+  },
+});
