ad6e8538a5032efe5b881b653c4348ccca89af2f
Edson José dos Santos <edsonj82@outlook.com>
2024-03-07 18:43:36 -0300

    Applying configuration in cypress mochawasome report

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-pr
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild")
 
 module.exports = defineConfig({
+  projectId: '4ic81v',
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
